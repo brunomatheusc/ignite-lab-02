36d41d010a63bd83cc77a5889dd11b2a239650d2
Bruno Matheus <bmcardoso@devgrid.co.uk>
2022-06-22 10:59:02 -0300

    Creating components and styling header component

---
+++ src/pages/Event.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Lesson from "../components/Lesson";
+import Player from "../components/Player";
+import Sidebar from "../components/Sidebar";
+
+export default function Event() {
+    return (
+        <>
+            <Header />
+            <Sidebar />
+            <Player />
+            <Lesson />
+        </>
+    );
+}
